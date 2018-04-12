b59727f92ed4c64948185f70dcc5cbafa08799fa
solomonwu <softwarewxw@gmail.com>
2018-04-12 14:02:18 +0800

    update

--- src/page/content/refreshcss/index.vue
+++ src/page/content/refreshcss/index.vue
@@ -6,7 +6,7 @@
 			<p>使用说明:</p>
 			<p>1、目录结构如下图:</p>
 			<img src="../../../assets/images/refreshcss.jpg">
-			<p class="p1">bs-config:browser-sync的配置</p>
+			<p class="p1">bs-config.js:browser-sync的配置</p>
 			<p class="p1">gulpfile.js:gulp的配置</p>
 			<p class="p1">index.html:入口文件，负责跳转到static</p>
 			<p class="p1">node-modules:npm依赖包</p>
@@ -14,7 +14,8 @@
 			<p class="p1">static:静态页面源代码</p>
 			<p>2、安装node、npm，并使用npm安装gulp和browser-sync，命令如下：</p>
 			<p style="border:1px solid #ddd;padding-left:5px;border-bottom:0px;">npm install gulp</p>
-			<p style="border:1px solid #ddd;padding-left:5px;border-top:0px;">npm install browser-sync</p>
+			<p style="border:1px solid #ddd;padding-left:5px;border-top:0px;border-bottom: 0px;">npm install browser-sync</p>
+			<p style="border:1px solid #ddd;padding-left:5px;border-top:0px;">npm install</p>
 			<p>3、cmd控制台中运行如下命令：</p>
 			<p style="border:1px solid #ddd;padding-left:5px;">gulp bs</p>
 			<p>即会自动打开index.html跳转到static中的index.html，这个可以自己在index.html中修改。</p>
